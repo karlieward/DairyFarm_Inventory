33af71c53bc6b8b0b716821a8755c876fa767919
= <casey.lee.black@gmail.com>
2025-11-11 16:37:02 -0700

    added a few things to indes.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Casey will work on adding some basic infastructure
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+let bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+app.set('view engine', 'ejs');
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+
+
+
+
+
+
+app.listen(
+
+)
