56301db445edd2db7a656e346b49548c19a8d60b
= <casey.lee.black@gmail.com>
2025-11-11 17:04:59 -0700

    added more structure to index.js

--- index.js
+++ index.js
@@ -2,21 +2,52 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-let bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host : process.env.DB_HOST,
+        user : process.env.DB_USER,
+        password : process.env.DB_PASSWORD,
+        database : process.env.DB_NAME,
+        port : process.env.DB_PORT
+    }
+});
 
 let app = express();
 
 app.set('view engine', 'ejs');
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
-
-
-
-
-
-
-
-app.listen(
-
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
 )
+app.use(express.urlencoded({extended: true}));
+
+app.use((req, res => {
+    if (req.path === '/' || req.path === '/login' || req.path === '/logout') {return next();}
+    if (req.session.isLoggedIn) {res.render('index');}
+    else {res.render('login', { error_message: "Please log in to access this page"});} 
+}));
+
+app.get("/", (req, res) => {
+    if (req.session.isLoggedIn) {        
+        res.render("index");
+    } 
+    else {
+      res.render("login", { error_message: "" });
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log("The server is listening");
+    console.log(`Server running on http://localhost:${PORT}`);
+})
