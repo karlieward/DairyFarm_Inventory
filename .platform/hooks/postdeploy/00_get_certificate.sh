7575f1ac11e474034a33cf3f28e20ea31c8cf5b9
karlieward <karlieward3@gmail.com>
2025-11-25 19:30:40 -0700

    CNAME

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d wardsdairies.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email karlie3@byu.edu
+sudo certbot -n -d wards-dairies.is404.net --nginx --agree-tos --email karlie3@byu.edu
