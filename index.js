c0d9963a90ef80d38c0492ab57deedc875bef396
karlieward <karlieward3@gmail.com>
2025-11-25 17:32:50 -0700

    encryption

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const db = require("knex")({
         user : process.env.RDS_USERNAME || "postgres",
         password : process.env.RDS_PASSWORD || "admin",
         database : process.env.RDS_DB_NAME || "project3",
-        port : process.env.RDS_PORT || 5432
+        port : process.env.RDS_PORT || 5432,
+        ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
 });
 
